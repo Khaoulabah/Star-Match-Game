028fe3a7d236ec06c03f4e0c3e7026d4da6d4f09
Khaoula Bahloul <khaoula@uw.edu>
2020-08-25 01:14:25 -0700

    Updates

---
+++ precache-manifest.ff15463e53e97192b1601e26e9a41b75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ce0fb91b048090f2e1e3e813f4abf5",
+    "url": "/Star-Match-Game/index.html"
+  },
+  {
+    "revision": "e9f3eaf3ec6d39ebf7f1",
+    "url": "/Star-Match-Game/static/css/main.36d36dea.chunk.css"
+  },
+  {
+    "revision": "19e979cefff1af0cf500",
+    "url": "/Star-Match-Game/static/js/2.1c95624f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Star-Match-Game/static/js/2.1c95624f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f3eaf3ec6d39ebf7f1",
+    "url": "/Star-Match-Game/static/js/main.b51f9e67.chunk.js"
+  },
+  {
+    "revision": "a507ca98a8557d94dc1c",
+    "url": "/Star-Match-Game/static/js/runtime-main.88e5420c.js"
+  }
+]);
